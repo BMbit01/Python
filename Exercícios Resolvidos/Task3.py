29be75652064e6e7946765c125828b480d6da761
Brenno MS <144502762+BMbit01@users.noreply.github.com>
2023-09-15 10:06:53 -0300

    Task3.py

---
+++ Exercícios Resolvidos/Task3.py
@@ -0,0 +1,8 @@
+#Crie um algoritmo que leia um número e mostre o seu dobro e triplo.
+
+v1 = int(input('Digite um numero: '))
+
+dobro = v1*2
+triplo = v1*3
+print('O dobro é {}'.format(dobro))
+print('O triplo é {}'.format(triplo))
