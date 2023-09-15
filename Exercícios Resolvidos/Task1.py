ca7fd50a1fe60deea6a04c17c66e72e815bdf301
Brenno MS <144502762+BMbit01@users.noreply.github.com>
2023-09-15 10:04:45 -0300

    Task1.py

---
+++ Exercícios Resolvidos/Task1.py
@@ -0,0 +1,8 @@
+#Crie um programa que leia dois números e mostre a soma entre eles.
+
+#Pedindo para o usuário inserir um valor
+v1 = int(input('Digite o primeiro valor: '))
+v2 = int(input('Digite o segundo valor: '))
+
+#Imprimindo a soma desses valores alocados em v1 e v2
+print(f'O resultado da soma é {v1 + v2}')
