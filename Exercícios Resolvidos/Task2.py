35bbebda84ad659af0a3c1d2054f29d11eeaf8a2
Brenno MS <144502762+BMbit01@users.noreply.github.com>
2023-09-15 10:06:09 -0300

    Task2.py

---
+++ Exercícios Resolvidos/Task2.py
@@ -0,0 +1,5 @@
+#Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+v1 = int(input('Digite um numero: '))
+
+print('O sucessor de {} é {} e o antecessor é {}'.format(v1, v1 + 1, v1 - 1))
