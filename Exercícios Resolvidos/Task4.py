4bbefc8f5f00f5c274eff0301779204ec20fe562
Brenno MS <144502762+BMbit01@users.noreply.github.com>
2023-09-15 10:08:17 -0300

    Task4.py

---
+++ Exercícios Resolvidos/Task4.py
@@ -0,0 +1,11 @@
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+# Solicita ao usuário que insira as duas notas
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+
+# Calcula a média das duas notas
+media = (nota1 + nota2) / 2
+
+# Exibe a média na tela
+print("A média do aluno é:", media)
